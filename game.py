376dd078018689aed54fdab8e9074c93f385fde2
Christoph Walpert <cjwalpert@gmail.com>
2018-05-19 09:41:52 +0200

    take_bet function created

--- game.py
+++ game.py
@@ -58,4 +58,30 @@ class Hand:
             self.value -= 10
             self.aces -= 1
 
-            
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet*2
+    
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+########FUNCTIONS#########
+
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
