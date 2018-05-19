ca62492fe62953737f7cc98bd59dfcd30b569569
Christoph Walpert <cjwalpert@gmail.com>
2018-05-19 09:19:49 +0200

    created Card class

--- game.py
+++ game.py
@@ -7,3 +7,14 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 
 playing = True
 
+#CARD CLASS 
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        
+    
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.suit)
+
